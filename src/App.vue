e983a07ceb1ae324a8eb5b02cbfee7b88dda143c
cnarendhar <narendhar.chiluka@bandgitechnologies.com>
2020-02-18 14:21:21 +0530

    full source

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  name : 'app',
+  components:{
+    Navbar
+  }
+}
+</script>
+<style>
+*{
+   font-family: 'Poppins', sans-serif;
+   border-radius: 0px !important
+}
+body{
+  background-color: #f5f3ef;
+}
+</style>
